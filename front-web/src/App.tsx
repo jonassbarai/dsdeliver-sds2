6fcf8c449b055b695b5827ab6c274502a8433d25
jonassbarai <jonas_Sbarai@hotmail.com>
2021-01-11 19:04:29 -0300

    add navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <h1>aaa</h1>
+    <div>
+      <Navbar/>
+    </div>
   );
 }
 
