f014b6ba6f4813c6b6c93cd4d5f52157068a9865
jonassbarai <jonas_Sbarai@hotmail.com>
2021-01-23 10:25:58 -0300

    add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Home/>
     </div>
   );
 }
